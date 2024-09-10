d5aa29ea2ada46a17818685f9a2c3df11f3ea60e
tolubabajide <tolubabajide2@utexas.edu>
2024-09-10 15:17:37 -0500

    updated quickstart

--- index.js
+++ index.js
@@ -3,30 +3,62 @@ import { uri } from "./secrets.js";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
 
-    console.log(
-      (await client.db("sample_mflix").collection("comments").find().toArray())
-        .length
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        const coll = client.db("sample_mflix").collection("comments");
+
+        // console.time();
+        // console.log(
+        //     (
+        //         await coll
+        //             .find()
+        //             .toArray()
+        //     ).length
+        // );
+        // console.timeEnd();
+
+        await coll.find().limit(10);
+
+        console.time();
+        console.log(
+            (
+                await coll
+                    .find()
+                    .limit(100)
+                    .toArray()
+            ).length
+        );
+        console.timeEnd();
+
+        console.time();
+        console.log(
+            (
+                await coll
+                    .find()
+                    .sort({ date: 1 })
+                    .limit(100)
+                    .toArray()
+            ).length
+        );
+        console.timeEnd();
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
 run().catch(console.dir);
